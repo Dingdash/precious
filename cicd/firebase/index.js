a6a24f2d6bf9e9fac02c53aca6636c13ae7ca054
Dingdash <youglh@yahoo.com>
2025-12-16 15:55:08 +0700

    trial

---
+++ cicd/firebase/index.js
@@ -0,0 +1,9 @@
+const admin = require('firebase-admin')
+const yaml = require('js-yaml')
+
+function main() {
+    console.log('ALKSDJFKLASFJKDLASFJKLDASFJKLAD')
+    console.log('INI BERHASIL')
+}
+
+main()
